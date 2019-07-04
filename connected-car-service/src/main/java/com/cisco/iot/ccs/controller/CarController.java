dba1814e98bbad6bbcab859b54307ee7bf26956e
sasravin <sasikumar.ravindran@gmail.com>
2019-07-04 18:43:57 +0530

    car comments mistake

--- connected-car-service/src/main/java/com/cisco/iot/ccs/controller/CarController.java
+++ connected-car-service/src/main/java/com/cisco/iot/ccs/controller/CarController.java
@@ -85,7 +85,7 @@ public class CarController {
 		}
 	}
 
-	@ApiOperation(value = "Get all the cars details  ", notes = "get models")
+	@ApiOperation(value = "Get all cars  ", notes = "get models")
 	@ApiResponses(value = { @ApiResponse(code = 404, message = "Not found"),
 			@ApiResponse(code = 400, message = "Bad request"),
 			@ApiResponse(code = 200, message = "Found", response = Car.class, responseContainer = "List"),
